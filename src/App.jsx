1b15662beca92122ff56c60eaeb76ea0a7adb1dd
Krupa2016 <krupagurav16@gmail.com>
2025-12-17 16:42:34 +0530

    linked about us

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Navbar from "./components/Navbar.jsx";
+import Navbar from "./components/navbar.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Events from "./pages/Events.jsx";
 import EventPage from "./components/EventPage.jsx";
