47dbdc6755bdb0165d364d78b03f67859781bfeb
ChiragS16 <csharma162005@gmail.com>
2025-12-19 12:51:37 +0530

    merged landing page

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Events from "./pages/Events.jsx";
 import EventPage from "./components/EventPage.jsx";
 import About from "./pages/About.jsx";
+import LandingPage from "./pages/LandingPage.jsx";
+import Footer from "./components/Footer.jsx"
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Events />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/About" element={<About />} />
         <Route path="/Events" element={<Events />} />
         <Route path="/events/:eventKey" element={<EventPage />} />
-      
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 }
